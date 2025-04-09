7fbb66fff79af57d412fa670df7929abad267bd7
Alexis Lozada <alexislozadasalinas11@gmail.com>
2025-04-09 15:22:48 -0600

    Arrglando problema de emails en produccion

--- services/notification-service/src/config/email.js
+++ services/notification-service/src/config/email.js
@@ -1,7 +1,7 @@
 const sgMail = require('@sendgrid/mail');
 
 // Configurar API key de SendGrid
-sgMail.setApiKey(process.env.SENDGRID_API_KEY || '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');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 // Configuración común para los emails
 const emailDefaults = {
